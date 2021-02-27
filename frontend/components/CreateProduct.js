a33885b360806ac00a24014e00f1a85c822dc3b1
Immo <mrerody@googlemail.com>
2021-02-27 10:21:19 +0100

    add styling to form, add custom invalidity message to image input

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import useForm from '../lib/useForm';
+import Form from './styles/Form';
 
 function CreateProduct(props) {
     const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -9,47 +10,63 @@ function CreateProduct(props) {
         description: 'nice shoes',
     });
     return (
-        <form>
-            <label htmlFor="name">
-                Name
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder="Name"
-                    value={inputs.name}
-                    onChange={handleChange}
-                />
-            </label>
-            <label htmlFor="price">
-                Price
-                <input
-                    type="number"
-                    id="price"
-                    name="price"
-                    placeholder="Price"
-                    value={inputs.price}
-                    onChange={handleChange}
-                />
-            </label>
-            <label htmlFor="description">
-                Description
-                <input
-                    type="text"
-                    id="description"
-                    name="description"
-                    placeholder="description"
-                    value={inputs.description}
-                    onChange={handleChange}
-                />
-            </label>
-            <button type="button" onClick={clearForm}>
-                Clear Form
-            </button>
-            <button type="button" onClick={resetForm}>
-                Reset Form
-            </button>
-        </form>
+        <Form
+            onSubmit={(e) => {
+                e.preventDefault();
+                console.log(inputs);
+            }}
+        >
+            <fieldset>
+                <label htmlFor="image">
+                    Image
+                    <input
+                        onInvalid={(e) =>
+                            e.target.setCustomValidity(
+                                'Please upload an image.'
+                            )
+                        }
+                        required
+                        type="file"
+                        id="image"
+                        name="image"
+                        onChange={handleChange}
+                    />
+                </label>
+                <label htmlFor="name">
+                    Name
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Name"
+                        value={inputs.name}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label htmlFor="price">
+                    Price
+                    <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        placeholder="Price"
+                        value={inputs.price}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label htmlFor="description">
+                    Description
+                    <textarea
+                        id="description"
+                        name="description"
+                        placeholder="Description"
+                        value={inputs.description}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button type="submit">+ Add Product</button>
+            </fieldset>
+        </Form>
     );
 }
 
