06629a0d8a4ce3f24dd5905edda9db2798bf714b
Immo <mrerody@googlemail.com>
2021-02-28 13:30:00 +0100

    add Pagination component

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -14,6 +14,7 @@ const GlobalStyles = createGlobalStyle`
         --black: #393939;
         --grey: #3A3A3A;
         --lightGrey: #e1e1e1;
+        --lightGray: #e1e1e1;
         --offWhite: #ededed;
         --maxWidth: 1000px;
         --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
---
+++ frontend/components/Pagination.js
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import Link from 'next/link';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import PaginationStyles from './styles/PaginationStyles';
+import DisplayError from './ErrorMessage';
+import { perPage } from '../config';
+
+const PAGINATION_QUERY = gql`
+    query PAGINATION_QUERY {
+        _allProductsMeta {
+            count
+        }
+    }
+`;
+
+function Pagination({ page }) {
+    const { data, error, loading } = useQuery(PAGINATION_QUERY);
+    if (loading) return 'Loading...';
+    if (error) return <DisplayError error={error} />;
+    const { count } = data._allProductsMeta;
+    const pageCount = Math.ceil(count / perPage);
+    return (
+        <PaginationStyles>
+            <Head>
+                <title>Sick Fits - Page {page} of ?</title>
+            </Head>
+            <Link href={`/products/${page - 1}`}>
+                <a aria-disabled={page <= 1}>⬅ Prev</a>
+            </Link>
+            <p>
+                Page {page} of {pageCount}
+            </p>
+            <p>{count} items total</p>
+            <Link href={`/products/${page + 1}`}>
+                <a aria-disabled={page >= pageCount}>Next ➡</a>
+            </Link>
+        </PaginationStyles>
+    );
+}
+
+Pagination.propTypes = {
+    page: PropTypes.number.isRequired,
+};
+
+export default Pagination;
