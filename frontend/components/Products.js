2d94eed69f14a1ec50692f9916a8d930a244a765
Immo <mrerody@googlemail.com>
2021-03-01 09:47:40 +0100

    adjust query for pagination values

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,11 +1,13 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { perPage } from '../config';
 import Product from './Product';
 
 export const ALL_PRODUCTS_QUERY = gql`
-    query ALL_PRODUCTS_QUERY {
-        allProducts {
+    query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+        allProducts(skip: $skip, first: $first) {
             id
             name
             price
@@ -26,8 +28,13 @@ const ProductsListStyles = styled.div`
     grid-gap: 60px;
 `;
 
-export default function Products() {
-    const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+function Products({ page }) {
+    const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+        variables: {
+            skip: (page - 1) * perPage,
+            first: perPage,
+        },
+    });
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
     return (
@@ -40,3 +47,9 @@ export default function Products() {
         </>
     );
 }
+
+Products.propTypes = {
+    page: PropTypes.number.isRequired,
+};
+
+export default Products;
