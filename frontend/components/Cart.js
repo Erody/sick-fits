2f17a9b569726cedb4474a64e1faebc69a4297b3
Immo <mrerody@googlemail.com>
2021-03-03 22:05:06 +0100

    add local state context for the cart state (open/closed), which needs to be available globally

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -6,14 +6,20 @@ import { useUser } from './User';
 import CartItem from './CartItem';
 import formatMoney from '../lib/formatMoney';
 import calcTotalPrice from '../lib/calcTotalPrice';
+import { useCart } from '../lib/cartState';
+import CloseButton from './styles/CloseButton';
 
-function Cart(props) {
+function Cart() {
     const me = useUser();
+    const { cartOpen, closeCart } = useCart();
     if (!me) return null;
     return (
-        <CartStyles open>
+        <CartStyles open={cartOpen}>
             <header>
                 <Supreme>{me.name}'s Cart</Supreme>
+                <CloseButton type="button" onClick={closeCart}>
+                    &times;
+                </CloseButton>
             </header>
             <ul>
                 {me.cart.map((cartItem) => (
