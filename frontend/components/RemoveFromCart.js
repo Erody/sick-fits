44fc7ead21ed1561c1edb160fd686ae46dde211e
Immo <mrerody@googlemail.com>
2021-03-04 10:43:58 +0100

    add remove from cart button to cart

---
+++ frontend/components/RemoveFromCart.js
@@ -0,0 +1,45 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const REMOVE_FROM_CART_MUTATION = gql`
+    mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
+        deleteCartItem(id: $id) {
+            id
+        }
+    }
+`;
+
+const BigButton = styled.button`
+    font-size: 3rem;
+    background: none;
+    border: 0;
+    &:hover {
+        color: red;
+        cursor: pointer;
+    }
+`;
+
+function RemoveFromCart({ id }) {
+    const [removeFromCart, { loading }] = useMutation(
+        REMOVE_FROM_CART_MUTATION,
+        { variables: { id } }
+    );
+    return (
+        <BigButton
+            title="Remove this item from cart"
+            disabled={loading}
+            onClick={removeFromCart}
+            type="button"
+        >
+            &times;
+        </BigButton>
+    );
+}
+
+RemoveFromCart.propTypes = {
+    id: PropTypes.string.isRequired,
+};
+
+export default RemoveFromCart;
