a6225348a8de6749749a180730e4b5e20c37d9e2
Immo <mrerody@googlemail.com>
2021-03-05 18:31:14 +0100

    create data types for orders

---
+++ backend/schemas/Order.ts
@@ -0,0 +1,17 @@
+import {
+    integer,
+    relationship,
+    select,
+    text,
+    virtual,
+} from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+
+export const Order = list({
+    fields: {
+        total: integer(),
+        items: relationship({ ref: 'OrderItem.order', many: true }),
+        user: relationship({ ref: 'User.orders' }),
+        charge: text(),
+    },
+});
