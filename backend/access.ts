3c522737272466478c2d8d79b29f63f5d145fb4f
Immo <mrerody@googlemail.com>
2021-03-08 08:33:58 +0100

    permissions access functions

--- backend/access.ts
+++ backend/access.ts
@@ -3,3 +3,16 @@ import { ListAccessArgs } from './types';
 export function isSignedIn({ session }: ListAccessArgs) {
     return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+    permissionsList.map((permissions) => [
+        permission,
+        function ({ session }: ListAccessArgs) {
+            return !!session?.data.role?.[permission];
+        },
+    ])
+);
+
+export const permissions = {
+    ...generatedPermissions,
+};
