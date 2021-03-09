ac6357147ef0ad58c84e08e98c0bc415b24e4df6
Immo <mrerody@googlemail.com>
2021-03-09 09:02:39 +0100

    update rules and permissions

--- backend/access.ts
+++ backend/access.ts
@@ -20,6 +20,9 @@ export const permissions = {
 
 export const rules = {
     canManageProducts({ session }: ListAccessArgs) {
+        if (!isSignedIn({ session })) {
+            return false;
+        }
         // do they have the permission of canManageProducts
         if (permissions.canManageProducts({ session })) {
             return true;
@@ -27,11 +30,47 @@ export const rules = {
         // do they own the product
         return { user: { id: session.itemId } };
     },
+    canOrder({ session }: ListAccessArgs) {
+        if (!isSignedIn({ session })) {
+            return false;
+        }
+        // do they have the permission of canManageProducts
+        if (permissions.canManageCart({ session })) {
+            return true;
+        }
+        // do they own the product
+        return { user: { id: session.itemId } };
+    },
+    canManageOrderItems({ session }: ListAccessArgs) {
+        if (!isSignedIn({ session })) {
+            return false;
+        }
+        // do they have the permission of canManageProducts
+        if (permissions.canManageCart({ session })) {
+            return true;
+        }
+        // do they own the product
+        return { order: { user: { id: session.itemId } } };
+    },
     canReadProducts({ session }: ListAccessArgs) {
+        if (!isSignedIn({ session })) {
+            return false;
+        }
         if (permissions.canManageProducts({ session })) {
             return true;
         }
         // they should only see products with the status available
         return { status: 'AVAILABLE' };
+    },
+    canManageUsers({ session }: ListAccessArgs) {
+        if (!isSignedIn({ session })) {
+            return false;
+        }
+        // do they have the permission of canManageProducts
+        if (permissions.canManageUsers({ session })) {
+            return true;
+        }
+        // do they own the product
+        return { id: session.itemId };
     },
 };
